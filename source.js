4b65af9119341d1978476da2c62e14732f2389df
eusebiops14 <64171953+eusebiops14@users.noreply.github.com>
2021-04-09 07:29:02 -0300

    inclusão página nossa historia

---
+++ source.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('a').forEach(link => {
+    link.onclick = function(e) {
+        e.preventDefault();
+
+        const conteudo = document.getElementById('conteudo')
+        fetch(link.href)
+            .then(resp => resp.text())
+            .then(html => conteudo.innerHTML = html)
+    }
+})
